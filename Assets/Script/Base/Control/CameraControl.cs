1cde2bb857693cd4957249b893bf750ce1765152
yanbj <yanbj@mail.jj.cn>
2024-11-04 21:14:37 +0800

    [DEV] 鼠标控制摄像机边界、拖拽、键盘控制摄像机

--- Assets/Script/Base/Control/CameraControl.cs
+++ Assets/Script/Base/Control/CameraControl.cs
@@ -3,7 +3,6 @@ namespace Base.Control
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
-
     public class CameraControl : MonoBehaviour
     {
         public bool IsInteracte = true;
@@ -42,6 +41,25 @@ namespace Base.Control
             SetCameraInitPos(Vector3.zero);
         }
 
+        private void Update()
+        {
+            if (IsInteracte)
+            {
+               if (!mouseIsDrag_)
+                {
+                    mouseInScreenBound();
+                }
+                keyCodeMoveCamera();
+                mouseDragCameraMove();
+                mouseScrollWheelChangeView();
+                // restrictCameraPosition();
+            }
+            else
+            {
+                if (mouseIsDrag_) mouseIsDrag_ = false;
+            }
+        }
+
         ///<summary>
         /// 设置相机移动系数
         ///</summary>
@@ -67,5 +85,127 @@ namespace Base.Control
         {
             camTrans_.position = new Vector3(pos.x, pos.y, camTrans_.position.z);
         }
+
+        ///<summary>
+        ///鼠标在屏幕边缘时移动相机
+        ///</summary>
+        private void mouseInScreenBound()
+        {
+            Vector3 v1 = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            //上移动
+            if (v1.y >= 1 - mouseOffset_)
+            {
+                camTrans_.Translate(Vector2.up * moveSpeed);
+            }
+            //下移动
+            if (v1.y <= mouseOffset_)
+            {
+                camTrans_.Translate(Vector2.down * moveSpeed);
+            }
+            //左移
+            if (v1.x <= mouseOffset_)
+            {
+                camTrans_.Translate(Vector2.left * moveSpeed);
+            }
+            //右移
+            if (v1.x >= 1 - mouseOffset_)
+            {
+                camTrans_.Translate(Vector2.right * moveSpeed);
+            }
+        }
+
+        /// <summary>
+        /// 按下对应键位时，移动相机
+        /// </summary>
+        private void keyCodeMoveCamera()
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                camTrans_.Translate(Vector2.up * moveSpeed);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                camTrans_.Translate(Vector2.down * moveSpeed);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                camTrans_.Translate(Vector2.left * moveSpeed);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                camTrans_.Translate(Vector2.right * moveSpeed);
+            }
+        }
+
+        /// <summary>
+        /// 鼠标拖动相机移动
+        /// </summary>
+        private void mouseDragCameraMove()
+        {
+            if (!mouseIsDrag_ && Input.GetMouseButtonDown(0))
+            {
+                mouseEndPos_ = Input.mousePosition;
+                mouseIsDrag_ = true;
+            }
+            if (mouseIsDrag_ && Input.GetMouseButton(0))
+            {
+                Vector3 curPos = Input.mousePosition;
+                Vector3 movePos = (curPos - mouseEndPos_) * dragSpeed;
+                camTrans_.position -= movePos;
+                mouseEndPos_ = curPos;
+            }
+            if (mouseIsDrag_ && Input.GetMouseButtonUp(0))
+            {
+                mouseIsDrag_ = false;
+            }
+        }
+
+        /// <summary>
+        /// 鼠标滚轮改变视野范围
+        /// </summary>
+        private void mouseScrollWheelChangeView()
+        {
+            mouseWheel_ = Input.GetAxis("Mouse ScrollWheel");//获取滚轮值的改变
+            //视野放大
+            if (mouseWheel_ < 0 && cam_.orthographicSize + zoomSpeed <= maxSize)
+            {
+                cam_.orthographicSize += zoomSpeed;
+            }
+            //视野缩小
+            else if (mouseWheel_ > 0 && cam_.orthographicSize - zoomSpeed >= minSize)
+            {
+                cam_.orthographicSize -= zoomSpeed;
+            }
+        }
+
+         /// <summary>
+        /// 限制相机位置
+        /// </summary>
+        private void restrictCameraPosition()
+    {
+        float cameraSize = cam_.orthographicSize - minSize;
+
+        float maxY = cameraSize * topPar_ + minSizeBound[0];
+        float minY = cameraSize * bottomPar_ + minSizeBound[1];
+        float minX = cameraSize * leftPar_ + minSizeBound[2];
+        float maxX = cameraSize * rightPar_ + minSizeBound[3];
+
+        if (camTrans_.position.y > maxY)
+        {
+            camTrans_.position = new Vector3(camTrans_.position.x, maxY, camTrans_.position.z);
+        }
+        if (camTrans_.position.y < minY)
+        {
+            camTrans_.position = new Vector3(camTrans_.position.x, minY, camTrans_.position.z);
+        }
+        if (camTrans_.position.x < minX)
+        {
+            camTrans_.position = new Vector3(minX, camTrans_.position.y, camTrans_.position.z);
+        }
+        if (camTrans_.position.x > maxX)
+        {
+            camTrans_.position = new Vector3(maxX, camTrans_.position.y, camTrans_.position.z);
+        }
+    }
     }
 }
